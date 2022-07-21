28ee40a32fb0065325c76818c1e6ee978a308d16
Attila Bordan <bordan_attila@hotmail.com>
2022-07-21 18:57:10 -0400

    Create random generator for felected criteria

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment code here
 
 var capital_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
-var numbers_list = [1, 2, 3,];
+var numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols_list = ["!", "@", "#", "$,", "%", "&", "*", "?"]
 var generatePassword = function(){
   var temp_password = [];
   var pswd_length = prompt("How long your password shoud be?");
@@ -11,22 +12,42 @@ var generatePassword = function(){
     alert("Password should be minimum of 8 characters and maximum of 128 characters.")
     pswd_length = prompt("How long your password shoud be?");
   } 
+  var lowercase_letter = confirm("Would you like lowercase letters?");
+  var numbers = confirm("Would you like numbers in your password?")
+  var symbols = confirm("Would you like symbols in your passworld?")
   
   if (capital_letter) {    
-    var random = Math.floor(Math.random() * capital_list.length);  
-    // console.log(random)  
-    for ( var i = 0; i < random; i++) {    
-      var select = Math.random() ;
-      select *= capital_list.length;
-      select = Math.floor(select);
-      console.log(select)
-      temp_password.push(capital_list[select])      
+    for ( var i = 0; i < pswd_length; i++) {    
+      var random = Math.floor(Math.random() * capital_list.length);  
+      var select_upper = random
+      temp_password.push(capital_list[select_upper])      
     }
   }
-  // var lowercase_letter = confirm("Would you like lowercase letters?")
+
+  if (lowercase_letter) {
+    for ( i = 0; i < pswd_length; i++) {
+      random = Math.floor(Math.random() * capital_list.length);
+      var select_lower = random
+      temp_password.push(capital_list[select_lower].toLowerCase())
+    }
+  }
+
+  if (numbers) {
+    for ( i = 0; i < pswd_length; i++) {
+      random = Math.floor(Math.random() * 9);
+      var select_number = random
+      temp_password.push(numbers_list[select_number])
+    }
+  }
+
+  if (symbols) {
+    for  (i = 0; i < pswd_length; i++) {
+      random = Math.floor(Math.random() * symbols_list.length);
+      var select_symbol = random
+      temp_password.push(symbols_list[select_symbol])
+    }
+  }
   
-  // var numbers = confirm("Would you like numbers in your password?")
-  // var symbols = confirm("Would you like symbols in your passworld?")
   console.log(temp_password)
   return temp_password;
 }
